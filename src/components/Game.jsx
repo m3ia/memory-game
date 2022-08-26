8a73b4b63cc20fcfcbfe8543a6a2a732c46a1144
m3ia <meianatividad@gmail.com>
2022-08-25 21:00:55 -0700

    Change photo names

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 
 const Game = (props) => {
+  const [game, setGame] = useState([]);
+  const [flippedCount, setFlippedCount] = useState(0);
+  const [flippedIndexes, setFlippedIndeces] = useState([]);
+
+  const photos = [
+    
+  ]
   return (
     <div className="game">
       <h1>Hi there</h1>
